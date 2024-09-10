30ca275c3af5f3cf7520f9602d02988429b43716
descipher <mike.laspina@gelidus.ca>
2024-09-09 22:40:46 -0500

    Fix protocol not being saved when manually set

--- components/secplus_gdo/select/gdo_select.h
+++ components/secplus_gdo/select/gdo_select.h
@@ -13,53 +13,73 @@
 #include "../secplus_gdo.h"
 #include "gdo.h"
 
-namespace esphome {
-namespace secplus_gdo {
+namespace esphome
+{
+  namespace secplus_gdo
+  {
 
-class GDOSelect : public select::Select, public Component {
- public:
-  void setup() override {
-    std::string value;
-    size_t index;
-    this->pref_ = global_preferences->make_preference<size_t>(this->get_object_id_hash());
-    if (!this->pref_.load(&index)) {
-      value = this->initial_option_;
-    } else if (!this->has_index(index)) {
-      value = this->initial_option_;
-    } else {
-      value = this->at(index).value();
-    }
+    class GDOSelect : public select::Select, public Component
+    {
+    public:
+      void setup() override
+      {
+        std::string value;
+        size_t index;
+        this->pref_ = global_preferences->make_preference<size_t>(this->get_object_id_hash());
+        if (!this->pref_.load(&index))
+        {
+          value = this->initial_option_;
+        }
+        else if (!this->has_index(index))
+        {
+          value = this->initial_option_;
+        }
+        else
+        {
+          value = this->at(index).value();
+        }
 
-    this->control(value);
-  }
-
-  void set_initial_option(const std::string &initial_option) { this->initial_option_ = initial_option; }
-
-  void update_state(gdo_protocol_type_t protocol) {
-    if (this->has_index(protocol)) {
-      std::string value = this->at(protocol).value();
-      if (this->has_state() && value != this->state) {
-        this->pref_.save(&protocol);
+        this->control(value);
       }
 
-      this->publish_state(value);
-    }
-  }
+      void set_initial_option(const std::string &initial_option) { this->initial_option_ = initial_option; }
 
- protected:
-  void control(const std::string &value) override {
-    auto idx = this->index_of(value);
-    if (idx.has_value()) {
-      gdo_protocol_type_t protocol = static_cast<gdo_protocol_type_t>(idx.value());
-      if (gdo_set_protocol(protocol) == ESP_OK) {
-        this->update_state(protocol);
+      void update_state(gdo_protocol_type_t protocol)
+      {
+        if (this->has_index(protocol))
+        {
+          std::string value = this->at(protocol).value();
+          if (this->has_state() && value != this->state)
+          {
+            this->pref_.save(&protocol);
+          }
+
+          this->publish_state(value);
+        }
       }
-    }
-  }
 
-  std::string initial_option_;
-  ESPPreferenceObject pref_;
-};
+    protected:
+      void control(const std::string &value) override
+      {
+        auto idx = this->index_of(value);
+        if (idx.has_value())
+        {
+          gdo_protocol_type_t protocol = static_cast<gdo_protocol_type_t>(idx.value());
+          if (gdo_set_protocol(protocol) == ESP_OK)
+          {
+            this->update_state(protocol);
+            this->update_state(protocol);
+            if (gdo_set_protocol(protocol) != ESP_OK)
+            {
+              vTaskDelay(pdMS_TO_TICKS(100));
+              App.safe_reboot();
+            }
+          }
+        }
 
-}  // namespace secplus_gdo
-}  // namespace esphome
+        std::string initial_option_;
+        ESPPreferenceObject pref_;
+      };
+
+    } // namespace secplus_gdo
+  } // namespace esphome
